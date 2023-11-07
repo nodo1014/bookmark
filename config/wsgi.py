bc9192e969f2469da86a6df3238b55e332e1ecca
nodo1014 <htmsejong@gmail.com>
2023-11-07 13:51:15 +0900

    wsgi 파이썬 애니웨어용

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
+path = "/home/jamukha/bookmark"
+
+if path not in sys.path:
+    sys.path.append(path)
+
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = get_wsgi_application()
+application = StaticFilesHandler(get_wsgi_application())
